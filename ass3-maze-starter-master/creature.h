10e156d269ced9d748369ab68e3cb1b95faaf2ec
tmoore53 <tyler.moore53@outlook.com>
2021-10-21 15:29:17 -0700

    Commiting assignment 3 template

---
+++ ass3-maze-starter-master/creature.h
@@ -0,0 +1,28 @@
+//
+//
+
+#ifndef ASS3_CREATURE_H
+#define ASS3_CREATURE_H
+
+#include "maze.h"
+#include <ostream>
+
+class Creature {
+public:
+  friend ostream &operator<<(ostream &Out, const Creature &Creature);
+
+private:
+  int Row;
+  int Col;
+
+public:
+  Creature(int Row, int Col);
+  string solve(Maze &Maze);
+  bool atExit(const Maze &Maze) const;
+  string goNorth(Maze &Maze);
+  string goSouth(Maze &Maze);
+  string goEast(Maze &Maze);
+  string goWest(Maze &Maze);
+};
+
+#endif //ASS3_CREATURE_H
---
+++ ass3-maze-starter-master/maze.h
@@ -0,0 +1,31 @@
+//
+// 
+//
+
+#ifndef ASS3_MAZE_H
+#define ASS3_MAZE_H
+
+#include <ostream>
+
+using namespace std;
+
+enum CELL { CLEAR, WALL, PATH, VISITED };
+
+class Maze {
+  friend ostream &operator<<(ostream &Out, const Maze &Maze);
+private:
+  const static int MAX_SIZE = 100;
+  char Field[MAX_SIZE][MAX_SIZE];
+  int Width, Height;
+  int ExitRow, ExitColumn;
+public:
+  explicit Maze(const string &FileName);
+  bool isClear(int Row, int Col) const;
+  void markAsPath(int Row, int Col);
+  void markAsVisited(int Row, int Col);
+  int getExitRow() const;
+  int getExitColumn() const;
+
+};
+
+#endif //ASS3_MAZE_H
