46a38ecbbb9fc2e351c55dcec54326e1439dc50b
tmoore53 <tyler.moore53@outlook.com>
2021-11-10 20:48:52 -0800

    Commiting signly linked list destructor

--- ass4-skiplist-starter-master/skiplist.cpp
+++ ass4-skiplist-starter-master/skiplist.cpp
@@ -1,6 +1,6 @@
 
-//modified by mashhadi on 14th feb to add an alternative op<<
-/* 
+// modified by mashhadi on 14th feb to add an alternative op<<
+/*
   Modified and implimented by Tyler A. Moore
 
 */
@@ -13,20 +13,18 @@
 
 using namespace std;
 
-ostream &operator<<(ostream &Out, const SkipList &SkipL)
-{
-  for (int Index = SkipL.maxLevel - 1; Index >= 0; Index--)
-  {
-    Out << "Level: " + (Index);
+ostream &operator<<(ostream &Out, const SkipList &SkipL) {
+  for (int Index = SkipL.maxLevel - 1; Index >= 0; Index--) {
+    Out << "Level: " + to_string(Index);
     Out << " -- ";
     SNode *Curr = SkipL.head;
-    while (Curr != nullptr)
-    {
+    while (Curr != nullptr) {
       Out << to_string(Curr->value) + ", ";
       Curr = Curr->forward;
+      if (Curr->down != nullptr)
+        Curr = Curr->down;
     }
     Out << "\n";
-    Curr = Curr->down;
   }
   return Out;
 }
@@ -66,20 +64,19 @@ ostream &operator<<(ostream &out, const SkipList &skip) {
 */
 
 // Skiplist Node Constructor
-SNode::SNode(int value) : value{value}, forward{nullptr}, backward{nullptr}, up{nullptr}, down{nullptr} {}
+SNode::SNode(int value)
+    : value{value}, forward{nullptr}, backward{nullptr}, up{nullptr},
+      down{nullptr} {}
 
 // how many forward/backward pointers it has
-int SNode::height() const
-{
+int SNode::height() const {
   int height{0};
-  while (this->forward != nullptr)
-  {
+  while (this->forward != nullptr) {
     height++;
   }
 
   int height2{0};
-  while (this->backward != nullptr)
-  {
+  while (this->backward != nullptr) {
     height2++;
   }
 
@@ -97,31 +94,27 @@ SNode **FrontGuards;
 // array of Depth SNode* objects as RearGuards linking levels
 SNode **RearGuards;
 
-SkipList::SkipList(int maxLevel, int probability) : maxLevel{maxLevel}, probability{probability}
-{
+SkipList::SkipList(int maxLevel, int probability)
+    : maxLevel{maxLevel}, probability{probability} {
   assert(maxLevel > 0 && probability >= 0 && probability < 100);
 }
 
-bool SkipList::shouldInsertAtHigher() const
-{
+bool SkipList::shouldInsertAtHigher() const {
   return rand() % 100 < probability;
 }
 
 bool SkipList::add(const vector<int> &values) { return true; }
 
-bool SkipList::add(int value)
-{
+bool SkipList::add(int value) {
   if (this->contains(value))
     return false;
 
-  if (head == nullptr)
-  {
+  if (head == nullptr) {
     SNode *newNode = new SNode(value);
     head = newNode;
   }
   SNode *curr = head;
-  while (curr != nullptr)
-  {
+  while (curr != nullptr) {
 
     curr = curr->forward;
   }
@@ -130,27 +123,28 @@ bool SkipList::add(int value)
   return true;
 }
 
-SkipList::~SkipList()
-{
+SkipList::~SkipList() {
   // need to delete individual nodes
   SNode *curr = head;
-  while (curr != nullptr)
-  {
+  while (curr != nullptr) {
+    SNode *next = curr->forward;
     delete curr;
-    curr = nullptr;
-    curr = head->forward;
-    if (head->forward == nullptr)
-    {
-      curr = head->down;
+    curr = next;
+    if (next->down == nullptr) {
+      curr = next->down;
     }
   }
+
+  // Head is already deleted
+  // delete head;
+  head = nullptr;
+  curr = nullptr;
 }
 
 bool SkipList::remove(int data) { return true; }
 
 // Given a SNode, place it before the given NextNode
-void SkipList::addBefore(SNode *NewNode, SNode *NextNode)
-{
+void SkipList::addBefore(SNode *NewNode, SNode *NextNode) {
   // Link next to node in front
   NewNode->forward = NextNode;
   // Link prev to node behind
@@ -173,7 +167,7 @@ void SkipList::addBefore(SNode *NewNode, SNode *NextNode)
 
 // get the node that would be before this data
 // at level-0
-//vector<SNode *> SkipList::getBeforeNodes(int data) const {
+// vector<SNode *> SkipList::getBeforeNodes(int data) const {
 //  vector<SNode *> v(maxLevel);
 //  return v;
 //}
@@ -190,7 +184,7 @@ SNode *SkipList::containsSNode(int data) const { return nullptr; }
 
 bool SkipList::contains(int data) const { return true; }
 
-//private methods to ease linking
+// private methods to ease linking
 void SkipList::connect2AtLevel(SNode *a, SNode *b, int level) {}
 
 void SkipList::connect3AtLevel(SNode *a, SNode *b, SNode *c, int level) {}
