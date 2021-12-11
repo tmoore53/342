c8911604bb1ebf2bd79d9ca2b512270ff7b3a922
tmoore53 <tyler.moore53@outlook.com>
2021-12-10 21:26:15 -0800

    Merge branch 'main' of github.com:tmoore53/342 into main

--- ass3-maze-starter-master/simplecompile.sh
+++ ass3-maze-starter-master/simplecompile.sh
@@ -23,14 +23,14 @@ clang++ -std=c++14 -Wall -Wextra -Wno-sign-compare *.cpp -g -o myprogram
 
 echo "*** running clang-tidy using options from .clang-tidy"
 clang-tidy --version
-clang-tidy creature.cpp -- -std=c++14
+clang-tidy myprogram -- -std=c++14
 
 echo "*** running myprogram"
 ./myprogram
 
 # valgrind will detect memory leaks
 echo "*** running with valgrind"
-valgrind ./myprogram.exe
+valgrind myprogram
 
 echo "*** cleaning up, deleting myprogram"
 rm myprogram
